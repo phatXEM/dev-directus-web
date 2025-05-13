7feea5d52f701eb16464df9fe789447fa0efbd08
phatXEM <phat@xem.edu.vn>
2025-05-13 15:36:11 +0700

    Update login with Facebook/Google

--- src/app/api/quick-auth/facebook/route.ts
+++ src/app/api/quick-auth/facebook/route.ts
@@ -5,23 +5,6 @@ export const POST = async (request: Request) => {
     const data = await request.json();
     const { accessToken } = data;
 
-    const userInfoResponse = await fetch(
-      `https://graph.facebook.com/v19.0/me?fields=email,name&access_token=${accessToken}`
-    );
-
-    if (!userInfoResponse.ok) {
-      throw new Error("Failed to fetch user info from Facebook");
-    }
-
-    const userInfo = await userInfoResponse.json();
-
-    // You might want to log or handle cases where email is undefined
-    if (!userInfo.email) {
-      return NextResponse.json({
-        error: "Email not provided by Facebook or permission not granted",
-      });
-    }
-
     const res = await fetch(
       process.env.NEXT_PUBLIC_FACEBOOK_REDIRECT_URI || "",
       {
@@ -30,12 +13,13 @@ export const POST = async (request: Request) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: userInfo.email,
-          name: userInfo.name || "",
+          code: accessToken,
         }),
       }
     );
+
     const { access_token, refresh_token, expires } = await res.json();
+
     return NextResponse.json({
       access_token,
       refresh_token,
